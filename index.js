2c53f46f9358f9d5792e485e32e1f75042a1d470
youssef8656 <youssefnady123456@gmail.com>
2024-12-18 14:01:36 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+
+const kofta=document.getElementById("kofta");
+kofta.onclick=function() {
+    window.open('kofta.html', '_blank');
+}
+const lobster=document.getElementById("lobster");
+lobster.onclick=function() {
+    window.open('lobster.html', '_blank');
+}
+const kabab=document.getElementById("kabab");
+kabab.onclick=function() {
+    window.open('kabab.html', '_blank');
+}
+const falafel=document.getElementById("falafel");
+falafel.onclick=function() {
+    window.open('falafel.html', '_blank');
+}
+const molokha=document.getElementById("molokha");
+molokha.onclick=function() {
+    window.open('molokha.html', '_blank');
+}
