b5c6edf46764f44c5aace4af204b856ac3e8991b
youssef8656 <youssefnady123456@gmail.com>
2024-12-22 17:12:09 +0200

    scroll-container

--- index.js
+++ index.js
@@ -19,3 +19,21 @@ const molokha=document.getElementById("molokha");
 molokha.onclick=function() {
     window.open('molokha.html', '_blank');
 }
+
+
+//auto scroll
+const scrollContainer = document.getElementById('scroll-container');
+
+function autoScroll() {
+    // Scroll the container by 200px horizontally
+    scrollContainer.scrollBy({ left: scrollContainer.offsetWidth , behavior: 'smooth' });
+
+    // If we've reached the end, reset to the start
+    if (scrollContainer.scrollLeft + scrollContainer.offsetWidth >= scrollContainer.scrollWidth-30) {
+
+        scrollContainer.scrollTo({ left: 0, behavior: 'smooth' });
+    }
+}
+
+// Run autoScroll every 5 seconds
+setInterval(autoScroll, 4000);
